6e8dc4f1d42ac23e073c5faf12236fa12b70df41
mohammed_bahr <mohammedbahr686@gmail.com>
2025-09-01 12:02:43 +0300

    add try and catch to the userSevice.js for more error handling

--- Services/userServices.js
+++ Services/userServices.js
@@ -2,13 +2,8 @@ import userModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
-export const register = async ({
-  firstName,
-  lastName,
-  email,
-  password,
-}) => {
+export const register = async ({ firstName, lastName, email, password }) => {
+  try {
     const findUser = await userModel.findOne({ email });
 
     if (findUser) {
@@ -24,32 +19,49 @@ export const register = async ({
     });
     await newUser.save();
 
-  return { data: generateJWT({ firstName, lastName, email }), statusCode: 200 };
-};
-
-
-export const login = async ({ email, password }) => {
-  const findUser = await userModel.findOne({ email });
-
-  if (!findUser) {
-    return { data: "Incorrect email or password!", statusCode: 400 };
-  }
-
-  const passwordMatch = await bcrypt.compare(password, findUser.password);
-  if (passwordMatch) {
     return {
-      data: generateJWT({
-        email,
-        firstName: findUser.firstName,
-        lastName: findUser.lastName,
-      }),
+      data: generateJWT({ firstName, lastName, email }),
       statusCode: 200,
     };
+  } catch (error) {
+    return {
+      data: "Something went wrong during registration.",
+      statusCode: 500,
+      error: error.message,
+    };
   }
+};
 
-  return { data: "Incorrect email or password!", statusCode: 400 };
+export const login = async ({ email, password }) => {
+  try {
+    const findUser = await userModel.findOne({ email });
+
+    if (!findUser) {
+      return { data: "Incorrect email or password!", statusCode: 400 };
+    }
+
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+    if (passwordMatch) {
+      return {
+        data: generateJWT({
+          email,
+          firstName: findUser.firstName,
+          lastName: findUser.lastName,
+        }),
+        statusCode: 200,
+      };
+    }
+
+    return { data: "Incorrect email or password!", statusCode: 400 };
+  } catch (error) {
+    return {
+      data: "Something went wrong during login.",
+      statusCode: 500,
+      error: error.message,
+    };
+  }
 };
 
 const generateJWT = (data) => {
-  return jwt.sign(data, '%%f!~LU|Jrwgdep[.z[[OGO:[{!6x(&b');
+  return jwt.sign(data, "%%f!~LU|Jrwgdep[.z[[OGO:[{!6x(&b");
 };
