62d162c9d8b50ea88e164256687f06ef5fb522d8
mohammed_bahr <mohammedbahr686@gmail.com>
2025-09-03 20:41:49 +0300

    installed tailwindcss and create register file but not started at it yet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import NavBar from './Components/NavBar'
+import Register from "./Pages/Register";
 function App() {
   return (
 <BrowserRouter>
   <NavBar />
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/register" element={<Register />} />
   </Routes>
 </BrowserRouter>
   )
